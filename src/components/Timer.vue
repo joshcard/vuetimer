03a591a92cdd2ca1dbf53cf9970a40fdd6da1abe
Josh Card <joshua.peter.card@gmail.com>
2021-08-18 15:54:22 -0600

    seconds time down but stop and zero.

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>Timer</div>
+  <div>{{timerMinutes}} : {{timerSeconds}}</div>
 </template>
 
 <script>
 export default {
   name: "Timer",
-  methods: {
-    
+  props: {
+    minutesLeft: {
+      default: 2
+    },
+    secondsLeft: {
+      default: 10
+    },
+  },
+  data() {
+    return {
+      timerMinutes: this.minutesLeft,
+      timerSeconds: this.secondsLeft,
+    }
+  },
+  watch: {
+    timerSeconds: {
+      handler(value) {
+        if (value > 0) {
+          setTimeout(() => {
+            this.timerSeconds--;
+          }, 1000);
+        }
+      },
+      immediate: true 
+    },
+    timerMinutes: {
+      handler(value) {
+        //this isn't setting the minute down one when the timerSeconds reaches 0
+        if (value > 0 && this.timerSeconds === 0) {
+          this.timerMinutes--;
+        }
+      },
+      immediate: false 
+    }
   }
 }
 </script>
@@ -14,3 +46,4 @@ export default {
 <style>
 
 </style>
+
